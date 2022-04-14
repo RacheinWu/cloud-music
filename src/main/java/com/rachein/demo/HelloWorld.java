814eed32004986b5eeacf039cfaab5061a02d77f
Rachein Wu <RacheinWu@outlook.com>
2022-04-14 22:47:21 +0800

    第一次提交

---
+++ src/main/java/com/rachein/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.rachein.demo;
+
+/**
+ * @author 计算机系 ITAEM 吴远健
+ * @Description
+ * @date 2022/4/14 22:44
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
